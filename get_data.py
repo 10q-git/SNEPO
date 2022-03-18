1b9c53f55e49cc65062d001243ac264c2b5856eb
10q-git <sengaev02@mail.ru>
2022-03-19 01:19:17 +0300

    Chanched spider

--- get_data.py
+++ get_data.py
@@ -1,3 +1,5 @@
+import re
+
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 from threading import Thread
@@ -13,7 +15,8 @@ names = set()
 email_addresses = set()
 phone_numbers = set()
 procs = []
-
+count_proc = 0
+visited_site = 0
 
 def valid_url(url):
     parsed = urlparse(url)
@@ -35,19 +38,23 @@ def website_links(url, flags):
         if href not in url:
             href = urljoin(url, href)
         parsed_href = urlparse(href)
-        # удалить параметры URL GET, фрагменты URL и т. д.
-        href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
-        if not valid_url(href):
-            # недействительный URL
-            continue
-        if href in int_url:
-            # уже в наборе
-            continue
-        if domain_name not in href:
-            continue
-        print(f"[*] Internal link: {href}")
-        urls.add(href)
-        int_url.add(href)
+        if "http" in parsed_href.scheme:
+            # удалить параметры URL GET, фрагменты URL и т. д.
+            href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
+            if not valid_url(href):
+                # недействительный URL
+                continue
+            if href in int_url:
+                # уже в наборе
+                continue
+            if domain_name not in href:
+                continue
+            print(f"[*] Internal link: {href}")
+
+            int_url.add(href)
+            #Отссеваем файлы для дальнейшего просмотра
+            if not re.search(r'\.[a-zA-Z]+$', href):
+                urls.add(href)
 
     if flags & 16:
         names = names.union(search_names.search(html_content.decode('UTF-8')))
@@ -55,23 +62,20 @@ def website_links(url, flags):
         email_addresses = email_addresses.union(search_emails.search(html_content.decode('UTF-8')))
     if flags & 4:
         phone_numbers = phone_numbers.union(search_phones.search(html_content.decode('UTF-8')))
-
     return urls
 
 
 def crawl(url, flags):
     global procs
-    try:
-        links = website_links(url, flags)
-    except:
-        links = set()
+    global count_proc
+    global visited_site
 
-    for link in links:
-        proc = Process(target=crawl, args=(link, flags))
-        procs.append(proc)
-        proc.start()
+    links = website_links(url, flags)
+    visited_site += 1
+    if visited_site < 2:
+        for link in links:
+            crawl(link, flags)
 
-    for proc in procs:
-        proc.join()
+
 
     return int_url, file_struct, names, email_addresses, phone_numbers
